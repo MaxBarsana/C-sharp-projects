47be67bb56a108512c9a3e36482e422085cff1a5
Max Barsana <34783148+MaxBarsana@users.noreply.github.com>
2018-02-02 11:56:14 -0800

    comitting ifAndFor folder

---
+++ ifAndFor/IfStatements(Expanded)/IfStatements(Expanded)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IfStatements_Expanded_.Global" Language="C#" %>
---
+++ ifAndFor/IfStatements(Expanded)/IfStatements(Expanded)/WebForm1.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IfStatements_Expanded_
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+            protected void Page_Load(object sender, EventArgs e)
+            {
+
+            }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+
+            /* Nested Boxes of code
+                if (firstCheckBox.Checked)
+                {
+                    if (secondCheckBox.Checked)
+                    {
+                        if (thirdCheckBox.Checked)
+                        {
+                            resultLabel.Text = "They're all checked";
+                        }
+                     }
+                 }
+             */
+
+
+            /*   Alternative if statements
+                if (!firstCheckBox.Checked) return;
+                if (!secondCheckBox.Checked) return;
+                if (!thirdCheckBox.Checked) return;
+                resultLabel.Text = "They're all checked!";
+            */
+
+
+            if (firstCheckBox.Checked &&
+                secondCheckBox.Checked &&
+                thirdCheckBox.Checked)
+                resultLabel.Text = "They're all checked!";
+
+        }
+    }
+}
