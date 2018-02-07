d9e03b286644099bd23d05f90608b7a892d08349
Max Barsana <34783148+MaxBarsana@users.noreply.github.com>
2018-02-07 11:32:32 -0800

    Classes concepts and excersices

---
+++ Classes/ClassesExplored/ClassesExplored/WebForm1.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ClassesExplored
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+            Car myNewCar = new Car();
+            
+            myNewCar.Make = "Oldsmobile";
+            myNewCar.Model = "Cutlas Supreme";
+            myNewCar.Year = 1986;
+            myNewCar.Color = "Silver";
+
+
+            //double myMarketValueOfCar = determineMarketValue(myNewCar);
+            double myMarketValueOfCar = myNewCar.DetermineMarketValue();
+
+            resultLabel.Text = String.Format("{0} - {1} - {2} - {3} - {4:C}", myNewCar.Make, myNewCar.Model, myNewCar.Year.ToString(), myNewCar.Color, myMarketValueOfCar);
+                
+
+
+        }
+
+
+        /*
+        private double determineMarketValue(Car car)
+        {
+            //double carValue = 100.0;
+            //Someday write code to go online and look up the car's value and retrieve value.
+
+
+            double carValue;
+
+            if (car.Year > 1990)
+                carValue = 10000.0;
+            else
+                carValue = 2000.0;
+            return carValue;
+
+        }
+
+    */
+
+    }
+
+    class Car
+    {
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+
+        public double DetermineMarketValue()
+        {
+            //double carValue = 100.0;
+            //Someday write code to go online and look up the car's value and retrieve value.
+
+
+            double carValue;
+
+            if (this.Year > 1990)
+                carValue = 10000.0;
+            else
+                carValue = 2000.0;
+            return carValue;
+
+        }
+    }
+
+}
